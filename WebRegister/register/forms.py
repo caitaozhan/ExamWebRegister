60e09491d96e480ea3ed20e297cb58b4f422cb59
sssoa <sssoa@icloud.com>
2016-04-19 00:13:16 +0800

    细节修正

--- WebRegister/register/forms.py
+++ WebRegister/register/forms.py
@@ -1,13 +1,6 @@
 from django import forms
 
 
-# class LoginForm(forms.Form):
-#     username = forms.CharField(label="用户名", max_length=10,
-#                                widget=forms.TextInput(attrs={'class': 'form-control'}))
-#     password = forms.CharField(label="密码", max_length=30,
-#                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
-
 class ExamForm(forms.Form):
     subject = forms.CharField(label="课程", max_length=30,
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
--- WebRegister/register/models.py
+++ WebRegister/register/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.utils import timezone
-# from accounts.models import Student
-
 
 class ExamInfoModel(models.Model):
     subject = models.CharField(max_length=30)   # 科目名称
