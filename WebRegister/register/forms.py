5b5dc2b9e9e0e308c0278bbece075658f3172c6a
caitaozhan <caitaozhan@163.com>
2016-04-19 19:43:09 +0800

    delete 'place' column in ExamInfoModel

--- WebRegister/register/forms.py
+++ WebRegister/register/forms.py
@@ -6,7 +6,7 @@ class ExamForm(forms.Form):
                               widget=forms.TextInput(attrs={'class': 'form-control'}))
     time = forms.DateTimeField(label="考试时间",
                                widget=forms.DateTimeInput(attrs={'class': 'form-control'}))
-    place = forms.CharField(label="考试地点", max_length=50,
-                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # place = forms.CharField(label="考试地点", max_length=50,
+                           # widget=forms.TextInput(attrs={'class': 'form-control'}))
     fee = forms.IntegerField(label="报考费用",
                              widget=forms.NumberInput(attrs={'class': 'form-control'}))
--- WebRegister/register/models.py
+++ WebRegister/register/models.py
@@ -6,13 +6,13 @@ class ExamInfoModel(models.Model):
     subject = models.CharField(verbose_name="科目名称", max_length=30)
     exam_time = models.DateTimeField(verbose_name="考试开始时间")
     exam_time_end = models.DateTimeField(verbose_name="考试结束时间", default=timezone.now())
-    place = models.CharField(verbose_name="考试地点", max_length=50)
+    # place = models.CharField(verbose_name="考试地点", max_length=50)
     register_deadline = models.DateTimeField(verbose_name="报名截至时间", default=timezone.now)
     fee = models.IntegerField(verbose_name="考试费用")
     notes = models.CharField(verbose_name="备注", max_length=512, default="")
 
     class Meta:
-        unique_together = (("subject", "exam_time", "place"),)  # 科目+时间+地点 -> 唯一确定一场考试
+        unique_together = (("subject", "exam_time"),)  # 科目+时间+地点 -> 唯一确定一场考试
     # django 不支持 multi-column primary key, unique_together 是一种可行的“替代”方案
         # db_table = '"考试信息"'
 
